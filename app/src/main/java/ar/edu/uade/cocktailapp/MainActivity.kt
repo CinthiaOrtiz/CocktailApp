858ea534ee59dffad1f4a0767c91b5c2dc9ae837
CinthiaOrtiz <cinthiaortiz2002@hotmail.com>
2025-05-12 16:32:04 -0300

    comienzos

--- app/src/main/java/ar/edu/uade/cocktailapp/MainActivity.kt
+++ app/src/main/java/ar/edu/uade/cocktailapp/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : ComponentActivity() {
                         cocktailList = getCocktailList(),
                         modifier = Modifier.padding(innerPadding)
                         //uiuiui
+                        // bkjnjkjk
                     )
                 }
             }
